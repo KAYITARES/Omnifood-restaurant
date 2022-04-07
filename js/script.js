e08d74f6114ad0e49b9483ff475dce5da5f17461
KAYITARES <cyntkayitare@example.com>
2022-04-07 16:12:26 +0200

    adding icon

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ allLinks.forEach(function (link) {
     const href = link.getAttribute("href");
     //Scroll to thre top
     if (href === "#")
-      window.screenTop({
+      window.scrollTo({
         top: 0,
         behavior: "smooth",
       });
@@ -37,8 +37,32 @@ allLinks.forEach(function (link) {
   });
 });
 
+//////////////////////////////////////////////////////////
+// STATIC SCROLL
+///////////////////////////////////////////////////////////
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    //In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
 // Fixing flexbox gap property missing in some Safari versions
-function checkFlexGap() {
+(function checkFlexGap() {
   var flex = document.createElement("div");
   flex.style.display = "flex";
   flex.style.flexDirection = "column";
@@ -53,7 +77,7 @@ function checkFlexGap() {
   console.log(isSupported);
 
   if (!isSupported) document.body.classList.add("no-flexbox-gap");
-}
+})();
 checkFlexGap();
 
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
